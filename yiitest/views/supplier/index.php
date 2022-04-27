09eca42045dd8c086441d0a7933cdd26d5364fcb
王俊知 <wonengbo@163.com>
2022-04-27 17:39:01 +0800

    init

---
+++ yiitest/views/supplier/index.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use app\models\Supplier;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\SupplierSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Suppliers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="supplier-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Supplier', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            [
+                'name' => 'id',
+                'class' => 'yii\grid\CheckboxColumn',
+            ],
+            'id',
+            'name',
+            'code',
+            [
+                'attribute' => 't_status',
+                'filter' => $searchModel->getTStatusList(),
+            ],
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Supplier $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
